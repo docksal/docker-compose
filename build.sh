cfab46370a1f295583de17f8ecc2e036c8facce1
Sergey Zabolotny <sergey.zabolotny@ffwagency.com>
2019-11-27 11:39:38 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-COMPOSER_VERSION="1.25.0"
+COMPOSER_VERSION="1.24.1"
 pwd=$(pwd)
 git clone -b ${COMPOSER_VERSION} https://github.com/docker/compose.git /tmp/docker-compose
 cp linux-alpine /tmp/docker-compose/script/build/
 cd /tmp/docker-compose/
 ./script/build/linux-alpine
-mkdir -p ${pwd}/bin/alpine/${COMPOSER_VERSION}
-cp dist/docker-compose-Linux-$(uname -m) ${pwd}/bin/alpine/${COMPOSER_VERSION}/docker-compose
+mkdir -p ${pwd}/bin/alpine/
+cp dist/docker-compose-Linux-$(uname -m) ${pwd}/bin/alpine/docker-compose
 cd ${pwd}
 rm -rf /tmp/docker-compose/
+
+git add -A
+git commit -m "Build version ${COMPOSER_VERSION}"
+git tag v${COMPOSER_VERSION}
+git push origin v${COMPOSER_VERSION}
